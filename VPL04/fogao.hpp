98afb7b68391698117c0959dcc651cb69586d512
Pedro <pcosme555@gmail.com>
2022-09-21 10:43:05 -0300

    Headers de geladeira e fogão adicionados

--- VPL04/fogao.hpp
+++ VPL04/fogao.hpp
@@ -5,10 +5,24 @@
 
 class Fogao {
     private:
-    // Adicione suas variaveis globais aqui
+
+    int _numero_de_queimadores;
+    int _capacidade_forno;
 
     public:
     // Adicione a assinatura dos seus metodos / construtores aqui
+    //Construtor
+    Fogao(int numero_de_queimadores, int capacidade_forno);
+    //Destrutor
+    ~Fogao();
+    //métodos
+    
+    int get_numero_de_queimadores();
+
+    int get_capacidade_forno();
+
+
+
 };
 
 #endif
--- VPL04/geladeira.hpp
+++ VPL04/geladeira.hpp
@@ -6,9 +6,16 @@
 class Geladeira {
     private:
     // Adicione suas variaveis globais aqui
+    int _capacidade_em_litros;
+    int _numero_de_portas;
 
     public:
     // Adicione a assinatura dos seus metodos / construtores aqui
+    Geladeira(int capacidade_em_litros, int numero_de_portas);
+    ~Geladeira();
+
+    int get_capacidade_em_litros();
+    int get_numero_de_portas();
 
 };
 
