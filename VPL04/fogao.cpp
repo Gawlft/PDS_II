4836e5be0c54a5f988a642465d941294c9cddd89
Gawlft <pcosme555@gmail.com>
2022-09-22 08:30:32 -0300

    fogao done

--- VPL04/fogao.cpp
+++ VPL04/fogao.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
 #include "fogao.hpp"
 
 // Adicione seus metodos / construtores aqui
+
+Fogao::Fogao(int numero_de_queimadores, int capacidade_forno){
+    _numero_de_queimadores = numero_de_queimadores;
+    _capacidade_forno = capacidade_forno;
+};
+
+Fogao::~Fogao(){
+
+};
+
+int Fogao::get_numero_de_queimadores(){
+    return _numero_de_queimadores;
+}
+
+int Fogao::get_capacidade_forno(){
+    return _capacidade_forno;
+}
